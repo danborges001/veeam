9e03e7b3d56f6d98f3e7117b94b46d055e55456e
danborges001 <danborges78@gmail.com>
2015-05-15 12:41:48 -0700

    changed ? to where-object
    ? is an alias for where-object changed to make script more readable

--- set_job_retention.ps1
+++ set_job_retention.ps1
@@ -1,7 +1,7 @@
 param(
 [string]$RetentionLength
 )
-$JobArray = Get-VBRJob | ? {$_.jobtype -eq "Backup"} | Select-Object -Property name,@{N="Target repository"; E={$_.GetTargetRepository().name}} | Where-Object {$_."Target repository" -Like "Default*"} 
+$JobArray = Get-VBRJob | Where-Object {$_.jobtype -eq "Backup"} | Select-Object -Property name,@{N="Target repository"; E={$_.GetTargetRepository().name}} | Where-Object {$_."Target repository" -Like "Default*"} 
 ForEach ($Job in $JobArray)
 	{
 	Get-VBRJob -Name $Job.name | Set-VBRJobAdvancedOptions -EnableIntegrityChecks $True -RetainDays $RetentionLength
